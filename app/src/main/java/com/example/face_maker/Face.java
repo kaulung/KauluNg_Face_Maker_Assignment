c2007e3a9a9f67c17ed59cf5269b0b02a7878503
kaulu <ngg21@up.edu>
2020-10-06 13:23:05 -1000

    Added methods for drawing features of the face

--- app/src/main/java/com/example/face_maker/Face.java
+++ app/src/main/java/com/example/face_maker/Face.java
@@ -17,12 +17,14 @@ import android.view.SurfaceView;
 
 public class Face extends SurfaceView {
 
-    int skinColor;
-    int eyeColor;
-    int hairColor;
-    int hairStyle;
-    Paint facePaint = new Paint();
-    Paint eyePaint = new Paint();
+    private int skinColor = 0;
+    private int eyeColor = 0;
+    private int hairColor = 0;
+    private int mouthColor = 0;
+    private int hairStyle;
+    private Paint featurePaint = new Paint();
+
+
 
 
     //Constructor for a Face object
@@ -30,34 +32,71 @@ public class Face extends SurfaceView {
         super(context,att);
         setWillNotDraw(false);
         setBackgroundColor(Color.WHITE);
-        skinColor = randomize(25);
-        eyeColor = randomize(25);
-        hairColor = randomize(25);
-        hairStyle = randomize(3);
+        randomize();
+    }
 
+    public void setSkinColor(int red, int green, int blue) {
+        skinColor = Color.rgb(red, green, blue);
+    }
+
+    public void setEyeColor(int red, int green, int blue){
+        eyeColor = Color.rgb(red, green, blue);
+    }
+
+    public void setHairColor(int red, int green, int blue){
+        hairColor = Color.rgb(red, green, blue);
+    }
+
+    public void setMouthColor(int red, int green, int blue){
+        mouthColor = Color.rgb(red,green,blue);
     }
 
     //Method to generate random numbers
-    public int randomize(int range){
+    public int randomize(){
         int n = 0;
-        n = (int)(Math.random()*range);
+        n = (int)(Math.random()*10);
         return n;
     }
 
+
+
+    public void drawFace(Canvas c){
+        featurePaint.setColor(skinColor);
+        featurePaint.setStyle(Paint.Style.FILL);
+        c.drawCircle(300.0f, 300.0f, 200.0f, featurePaint);
+    }
+
+    public void drawEyes(Canvas c){
+        featurePaint.setColor(eyeColor);
+        featurePaint.setStyle(Paint.Style.FILL);
+        c.drawCircle(100.0f, 200.0f, 100.0f, featurePaint);
+        c.drawCircle(500.0f, 200.0f, 100.0f, featurePaint);
+    }
+
+    public void drawMouth(Canvas c){
+        featurePaint.setColor(mouthColor);
+        featurePaint.setStyle(Paint.Style.FILL);
+    }
+
+    public void drawHair(Canvas c){
+        featurePaint.setColor(hairColor);
+        featurePaint.setStyle(Paint.Style.FILL);
+    }
+
+
+
     @ Override
     public void onDraw(Canvas c){
-        facePaint.setColor(skinColor);
-        facePaint.setStyle(Paint.Style.FILL);
-        eyePaint.setColor(eyeColor);
-        eyePaint.setStyle(Paint.Style.FILL);
-        c.drawCircle(50.0f, 50.0f, 50.0f, facePaint);
-        c.drawCircle(20.0f, 30.0f, 10.0f, eyePaint);
-        c.drawCircle(40.0f, 30.0f, 10.0f, eyePaint);
+        //Call drawFace method, which draws the face
+        drawFace(c);
+        //Call drawEyes method, which draws the eyes
+        drawEyes(c);
+        //Call drawMouth method, which draws the mouth
+        drawMouth(c);
+        //Call drawHair method, which draws the hair
         drawHair(c);
     }
 
-    public void drawHair(Canvas canvas){
 
-    }
 
 }
